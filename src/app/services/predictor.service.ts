4fbd6102d05e28d78cac59a9463aaf5eaade63ce
anthonyykh <anthonykh01.ak@gmail.com>
2022-11-27 16:41:21 +0200

    added 3 services for each of the needed apis

--- src/app/services/predictor.service.ts
+++ src/app/services/predictor.service.ts
@@ -10,7 +10,7 @@ import { DogResponse } from '../tab1/tab1.module';
 
 
 export class PredictorService {
-  
+
   public getDog(): Observable<DogResponse> {
     return this.httpClient.get<DogResponse>('https://dog.ceo/api/breeds/image/random');
   }
